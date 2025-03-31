6d88971fa947c910ea7c299ba0418914f4bc22fd
BingrongSu <robertsu1121@outlook.com>
2025-03-31 21:32:20 +0800

    Update Datagen

--- src/main/java/net/robert/soulland/datagen/loot/ModBlockLootTables.java
+++ src/main/java/net/robert/soulland/datagen/loot/ModBlockLootTables.java
@@ -16,7 +16,7 @@ import net.robert.soulland.block.ModBlocks;
 import java.util.Set;
 
 public class ModBlockLootTables extends BlockLootSubProvider {
-    protected ModBlockLootTables() {
+    public ModBlockLootTables() {
         super(Set.of(), FeatureFlags.REGISTRY.allFlags());
     }
 
