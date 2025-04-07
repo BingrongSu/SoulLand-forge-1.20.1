e59513d28bc1e202b179bf8d7e72bace498b02b0
BingrongSu <robertsu1121@outlook.com>
2025-04-07 15:16:39 +0800

    Recipe Screen Complete

--- src/main/java/net/robert/soulland/item/custom/pill_recipes/IncreaseSoulPowerPR.java
+++ src/main/java/net/robert/soulland/item/custom/pill_recipes/IncreaseSoulPowerPR.java
@@ -1,9 +1,19 @@
 package net.robert.soulland.item.custom.pill_recipes;
 
-import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.screens.inventory.BookViewScreen;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.nbt.ListTag;
+import net.minecraft.nbt.StringTag;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResultHolder;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
 import net.minecraft.world.level.Level;
+import net.robert.soulland.SoulLand;
 
 public class IncreaseSoulPowerPR extends Item {
     private final int level;
@@ -18,8 +28,24 @@ public class IncreaseSoulPowerPR extends Item {
     }
 
     @Override
-    public ItemStack finishUsingItem(ItemStack pStack, Level pLevel, LivingEntity pLivingEntity) {
-        // TODO Create Screen
-        return super.finishUsingItem(pStack, pLevel, pLivingEntity);
+    public InteractionResultHolder<ItemStack> use(Level pLevel, Player pPlayer, InteractionHand pUsedHand) {
+        if (pLevel.isClientSide) {
+            ItemStack book = new ItemStack(Items.WRITTEN_BOOK);
+            CompoundTag tag = book.getOrCreateTag();
+
+            tag.putString("title", "配方大全");
+            tag.putString("author", SoulLand.MOD_ID);
+
+            ListTag pages = new ListTag();
+            pages.add(StringTag.valueOf(Component.Serializer.toJson(Component.translatable("increase_soul_power_pill_recipe.content.p1"))));
+            pages.add(StringTag.valueOf(Component.Serializer.toJson(Component.translatable("increase_soul_power_pill_recipe.content.p2"))));
+            pages.add(StringTag.valueOf(Component.Serializer.toJson(Component.translatable("increase_soul_power_pill_recipe.content.p3"))));
+            pages.add(StringTag.valueOf(Component.Serializer.toJson(Component.translatable("increase_soul_power_pill_recipe.content.p4.lv%d".formatted(level)))));
+
+            tag.put("pages", pages);
+            Minecraft.getInstance().setScreen(new BookViewScreen(BookViewScreen.BookAccess.fromItem(book)));
+
+        }
+        return InteractionResultHolder.sidedSuccess(pPlayer.getItemInHand(pUsedHand), pLevel.isClientSide());
     }
 }
