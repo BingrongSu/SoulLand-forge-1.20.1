c10f5d3064b1780ebc422bb5ceea28ceb9acdb11
BingrongSu <robertsu1121@outlook.com>
2025-04-08 19:01:45 +0800

    Finish Alchemy Furnace

--- src/main/java/net/robert/soulland/screen/AlchemyFurnaceScreen.java
+++ src/main/java/net/robert/soulland/screen/AlchemyFurnaceScreen.java
@@ -29,16 +29,16 @@ public class AlchemyFurnaceScreen extends AbstractContainerScreen<AlchemyFurnace
     @Override
     protected void renderBg(GuiGraphics guiGraphics, float pPartialTick, int pMouseX, int pMouseY) {
         RenderSystem.setShader(GameRenderer::getPositionTexShader);
-        RenderSystem.setShaderColor(1.0F, 1.0F, 1.0F, 0.8F);
+        RenderSystem.setShaderColor(1.0F, 1.0F, 1.0F, 1.0F);
         RenderSystem.setShaderTexture(0, TEXTURE);
         int x = (width - imageWidth) / 2;
         int y = (height - imageHeight) / 2;
 
         guiGraphics.blit(TEXTURE, x, y, 0, 0, imageWidth, imageHeight);
 
-        RenderSystem.setShaderColor(1.0F, 1.0F, 1.0F, 1.0F);
         renderProgressArrow(guiGraphics, x, y);
         renderFireIcon(guiGraphics, x, y);
+//        RenderSystem.setShaderColor(1.0F, 1.0F, 1.0F, 1.0F);
     }
 
     private void renderProgressArrow(GuiGraphics guiGraphics, int x, int y) {
