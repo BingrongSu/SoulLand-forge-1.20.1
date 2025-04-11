878aabd5736f2abcdb35c202d0a4b0719156fc61
BingrongSu <robertsu1121@outlook.com>
2025-04-11 23:37:19 +0800

    Update algorithmTest.java

--- src/main/java/net/robert/soulland/helper/algorithmTest.java
+++ src/main/java/net/robert/soulland/helper/algorithmTest.java
@@ -1,27 +1,32 @@
 package net.robert.soulland.helper;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class algorithmTest {
     public static void main(String[] args) {
 
-        List<Double> list1 = List.of(3d, 1d, 2d);
-        List<Double> list2 = new java.util.ArrayList<>(List.of(2d, 1d, 3d));
-        boolean b = true;
-        for (int i = 0; i < 3; i++) {
-            boolean fit = false;
-            for (int j = 0; j < 3-i; j++) {
-                if (list2.get(j).equals(list1.get(i))) {
-                    list2.remove(j);
-                    fit = true;
-                    break;
-                }
-            }
-            if (!fit) {
-                b = false;
-                break;
-            }
-        }
-        System.out.println(b);
+//        List<Double> list1 = List.of(3d, 1d, 2d);
+//        List<Double> list2 = new java.util.ArrayList<>(List.of(2d, 1d, 3d));
+//        boolean b = true;
+//        for (int i = 0; i < 3; i++) {
+//            boolean fit = false;
+//            for (int j = 0; j < 3-i; j++) {
+//                if (list2.get(j).equals(list1.get(i))) {
+//                    list2.remove(j);
+//                    fit = true;
+//                    break;
+//                }
+//            }
+//            if (!fit) {
+//                b = false;
+//                break;
+//            }
+//        }
+//        System.out.println(b);
+
+        ArrayList<Integer> cba = new ArrayList<>(List.of(1, 2, 3, 4));
+
+
     }
 }
